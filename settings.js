8d2dbab932406d131d926f5b21438b9577b83c59
❤️‍🔥සිතුම් ශ්‍රීනාත්❤️‍🔥 <Sithumsrinath8278@gmail.com>
2025-01-27 22:39:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-//👉 Put Your Session Id 🐣 👈කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
+//👉LAKA-MD=NgpWDLYZ#xSJE3nZweTnfdxHvesAf9MWGmupAd3a1K73-2Fn3SjM👈කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
 SESSION_ID: process.env.SESSION_ID || "Put Your Session Id 🐣",
 //👉 Put Your Mongode Url 🖇️ 👈කියන එක කපලා අයින් කරලා ඔයාගේ Mongode Url එක දාන්න.
 MONGODB: process.env.MONGODB || "Put Your Mongode Url 🖇️",
